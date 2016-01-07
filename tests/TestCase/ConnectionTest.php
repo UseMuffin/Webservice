4ee209b57e96d232ee2bc08ce1918c2b83c8f26a
Marlin Cremers <m.cremers@cvo-technologies.com>
2016-01-07 14:52:14 +0100

    Add connection tests

--- tests/TestCase/ConnectionTest.php
+++ tests/TestCase/ConnectionTest.php
@@ -2,11 +2,44 @@
 namespace Muffin\Webservice\Test\TestCase;
 
 use Cake\TestSuite\TestCase;
+use Muffin\Webservice\Connection;
 
 class ConnectionTest extends TestCase
 {
-    public function testConstructor()
+
+    /**
+     * @var Connection
+     */
+    public $connection;
+
+    public function setUp()
     {
-        $this->markTestIncomplete('Not yet implemented.');
+        parent::setUp();
+
+        $this->connection = new Connection([
+            'name' => 'test',
+            'service' => 'Test'
+        ]);
+    }
+
+    /**
+     * @expectedException \Muffin\Webservice\Exception\MissingDriverException
+     */
+    public function testConstructorMissingDriver()
+    {
+        new Connection([
+            'name' => 'test',
+            'service' => 'MissingDriver'
+        ]);
+    }
+
+    /**
+     * @expectedException \Muffin\Webservice\Exception\MissingConnectionException
+     */
+    public function testConstructorNoDriver()
+    {
+        new Connection([
+            'name' => 'test',
+        ]);
     }
 }
