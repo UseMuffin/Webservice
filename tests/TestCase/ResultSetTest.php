f0349f5ed1089445eec1222fe25b44bc877bb2cf
David Yell <dyell@ukwebmedia.com>
2017-08-15 15:26:04 +0100

    Preserve the pagination data in the result set

--- tests/TestCase/ResultSetTest.php
+++ tests/TestCase/ResultSetTest.php
@@ -21,20 +21,24 @@ class ResultSetTest extends TestCase
     {
         parent::setUp();
 
-        $this->resultSet = new ResultSet([
-            new Resource([
-                'id' => 1,
-                'title' => 'Hello World'
-            ]),
-            new Resource([
-                'id' => 2,
-                'title' => 'New ORM'
-            ]),
-            new Resource([
-                'id' => 3,
-                'title' => 'Webservices'
-            ])
-        ], 6);
+        $this->resultSet = new ResultSet(
+            [
+                new Resource([
+                    'id' => 1,
+                    'title' => 'Hello World'
+                ]),
+                new Resource([
+                    'id' => 2,
+                    'title' => 'New ORM'
+                ]),
+                new Resource([
+                    'id' => 3,
+                    'title' => 'Webservices'
+                ])
+            ], 
+            6, 
+            ['count' => 6, 'prevPage' => false, 'nextPage' => false]
+        );
     }
 
     public function testCount()
@@ -57,6 +61,11 @@ class ResultSetTest extends TestCase
         $unserialized = unserialize(serialize($this->resultSet));
 
         $this->assertInstanceOf('\Muffin\Webservice\ResultSet', $unserialized);
+    }
+
+    public function testPagination()
+    {
+        $this->assertEquals(['count' => 6, 'prevPage' => false, 'nextPage' => false], $this->resultSet->pagination());
     }
 
     /**
