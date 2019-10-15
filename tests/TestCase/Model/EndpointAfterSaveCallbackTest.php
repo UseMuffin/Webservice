cf25aa2d99b3e1d12a407413938fdaf3bc6fca53
Hadry <hadry@protonmail.com>
2019-10-15 11:05:55 +0200

    [DONE] added test case

---
+++ tests/TestCase/Model/EndpointAfterSaveCallbackTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Muffin\Webservice\Test\TestCase\Model;
+
+use Cake\TestSuite\TestCase;
+use Muffin\Webservice\Connection;
+use Muffin\Webservice\Model\Resource;
+use Muffin\Webservice\Test\test_app\Model\Endpoint\CallbackEndpoint;
+
+class EndpointAfterSaveCallbackTest extends TestCase
+{
+    /**
+     * @var \Muffin\Webservice\Connection
+     */
+    public $connection;
+
+    /**
+     * @var Endpoint
+     */
+    public $endpoint;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->connection = new Connection([
+            'name' => 'test',
+            'service' => 'Test'
+        ]);
+        $this->endpoint = new CallbackEndpoint([
+            'connection' => $this->connection,
+            'primaryKey' => 'id',
+            'displayField' => 'title',
+            'schema' => [
+                'id' => ['type' => 'int'],
+                'title' => ['type' => 'string'],
+                'body' => ['type' => 'string'],
+            ]
+        ]);
+    }
+
+    /**
+     * Test afterSave return altered data
+     */
+    public function testAfterSave()
+    {
+        $resource = new Resource([
+            'id' => 4,
+            'title' => 'Loads of fun',
+            'body' => 'Woot'
+        ]);
+
+        $savedResource = $this->endpoint->save($resource);
+        $this->assertInstanceOf('\Muffin\Webservice\Model\Resource', $savedResource);
+        $this->assertEquals([
+            'id' => 4,
+            'title' => 'Loads of sun',
+            'body' => 'Woot'
+        ], $savedResource->toArray());
+
+        $newResource = $this->endpoint->get(4);
+        $this->assertInstanceOf('\Muffin\Webservice\Model\Resource', $newResource);
+        $this->assertEquals([
+            'id' => 4,
+            'title' => 'Loads of fun',
+            'body' => 'Woot'
+        ], $newResource->toArray());
+
+    }
+
+}
---
+++ tests/test_app/Model/Endpoint/CallbackEndpoint.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Muffin\Webservice\Test\test_app\Model\Endpoint;
+
+
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Muffin\Webservice\Model\Endpoint;
+
+class CallbackEndpoint extends Endpoint
+{
+
+    /**
+     * Test afterSave Callback
+     * @param Event $event
+     * @param EntityInterface $entity
+     * @param \ArrayObject $options
+     * @return bool|EntityInterface
+     */
+    public function afterSave(Event $event, EntityInterface $entity, \ArrayObject $options)
+    {
+        $event->stopPropagation();
+        if($entity->get('title')) {
+        $entity->set('title', 'Loads of sun');
+    }
+
+        return $entity;
+    }
+}
